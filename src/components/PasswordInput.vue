4dee626572e62516f42bb53818a16108faf4c15e
Nathã Lopes Zerbo <nathalopeszerbo7@gmail.com>
2025-06-16 14:44:05 -0300

    Entendendo como funciona o Quasar

---
+++ src/components/PasswordInput.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <q-input :type="isPsw ? 'password' : 'text'" v-model="value">
+    <template v-slot:append> 
+      <q-icon :name=" isPsw ? 'visibility' : 'visibility_off'" @click="isPsw = !isPsw"> 
+      </q-icon>
+    </template>
+  </q-input>
+
+</template>
+
+<script  lang="ts">
+import { computed, defineComponent, ref } from 'vue';
+  export default defineComponent({
+    name: 'PasswordInput',
+    props: {
+      modelValue: {
+        type:String,
+        required: true
+      }
+    },
+    setup(props, {emit}) {
+      const value = computed({
+        get: () => props.modelValue,
+        set: value => emit('update:modelValue', value)
+      })
+
+      const isPsw = ref<boolean>(true)
+
+      return{
+        value,
+        isPsw
+      }
+    }
+  })
+  
+</script>
--- src/pages/UserPage.vue
+++ src/pages/UserPage.vue
@@ -1,31 +1,71 @@
 <template>
   <q-page padding>
-    <q-form class="q-gutter-md"> 
-      <div>
-         <q-input model-value="" label="Nome" filled />
-      </div>
-     
-      <q-input model-value="" label="Data de Nascimento" filled class="data">
+    <q-form class="q-gutter-md flex column"> 
+      <q-input v-model="name" label="Nome" filled />
+
+      <q-input v-model="bornDate" mask="##/##/####" label="Data de Nascimento" filled class="data">
         <template v-slot:append>
           <q-icon name="event">
             <q-popup-proxy>
-              <q-date model-value="" />
+              <q-date v-model="bornDate" mask="DD/MM/YYYY"/>
             </q-popup-proxy>
           </q-icon>
         </template>
       </q-input>
-      <q-select model-value="" label="Gênero" filled :options="[
+      <q-select v-model="gender" label="Gênero" filled :options="[
           'Masculino',
           'Feminino'
       ]"> 
       </q-select>
-      <q-checkbox model-value="" label="Aceito os termos de uso"/>
-      <q-btn label="Salvar"/>
+      <PasswordInput v-model="password" />
+      <q-checkbox v-model="term" label="Aceito os termos de uso"/>
+      <q-btn label="Salvar" class="bg-blue-13 text-white" @click="onSave"/>
     </q-form>
   </q-page>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { useQuasar } from 'quasar'
+import PasswordInput from 'src/components/PasswordInput.vue'
+import { defineComponent, ref } from 'vue'
 
+export default defineComponent({
+  name: 'UserPage',
+  components: {
+    PasswordInput
+  },
+  setup() {
+    const { notify } = useQuasar()
+
+    const name = ref<string>('')
+    const bornDate = ref<string>('')
+    const gender = ref<string>('')
+    const term = ref<boolean>(false)
+    const password = ref<string>('')
+
+    const onSave = () => {
+      if (name.value  === '') {
+        notify({
+          message: 'Campo está vazio.',
+          type: 'negative'
+        })
+      } else {
+        notify({
+          message: `${name.value} salvo com sucesso.`,
+          type: 'positive'
+        })
+      }
+    }
+
+    return {
+      name,
+      bornDate,
+      gender,
+      term,
+      password,
+      onSave
+    }
+  }
+})
 </script>
 
